d69daf097f303324bf9a72640a359fea395f93eb
Do Hong Quang <quangteo06061999@gmail.com>
2024-10-30 08:30:31 +0900

    Initial commit

---
+++ src/main/java/com/example/quangproject/com/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.quangproject.com;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HelloController {
+	@GetMapping("/quang1")
+	public String index() {
+		return "XIN CHAO DO HONG QUANG";
+	}
+	
+}
